6f5051d8e54bfb7da004b087bfbf37e0b564c554
Trey Terrell <tterrell@princeton.edu>
2016-02-04 09:35:38 -0800

    Add generators and configuration.

---
+++ README.md
@@ -0,0 +1,3 @@
+= BookConcerns
+
+This project rocks and uses MIT-LICENSE.
---
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -0,0 +1,22 @@
+require 'rails/generators'
+
+class TestAppGenerator < Rails::Generators::Base
+  source_root "./spec/test_app_templates"
+
+  # if you need to generate any additional configuration
+  # into the test app, this generator will be run immediately
+  # after setting up the application
+
+  def install_curation_concerns
+    generate 'curation_concerns:install', '-f'
+  end
+
+  def install_engine
+    generate 'book_concerns:install'
+  end
+
+  def run_migrations
+    rake 'db:migrate'
+  end
+end
+
